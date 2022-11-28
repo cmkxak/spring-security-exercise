3183deabcd97f4513f9e925a2d2d1fc5d878325d
cmkxak <gcm466@naver.com>
2022-11-28 14:07:09 +0900

    [chore] : 오타 수정 및 Service 어노테이션 추가

--- src/main/java/likelion/springsecurityexercise/service/UserService.java
+++ src/main/java/likelion/springsecurityexercise/service/UserService.java
@@ -3,7 +3,9 @@ package likelion.springsecurityexercise.service;
 import likelion.springsecurityexercise.domain.dto.UserDto;
 import likelion.springsecurityexercise.domain.dto.UserJoinRequest;
 import likelion.springsecurityexercise.domain.dto.UserJoinResponse;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
     public UserDto join(UserJoinRequest joinRequest) {
         return new UserDto("","", "", "");
