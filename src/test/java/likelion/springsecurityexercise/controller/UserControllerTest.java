4e54b9658da4e9549172cb505dbb8adb51dbb224
cmkxak <gcm466@naver.com>
2022-11-29 14:37:13 +0900

    [add] : login fail Test Code

--- src/test/java/likelion/springsecurityexercise/controller/UserControllerTest.java
+++ src/test/java/likelion/springsecurityexercise/controller/UserControllerTest.java
@@ -1,9 +1,9 @@
 package likelion.springsecurityexercise.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import likelion.springsecurityexercise.domain.dto.UserDto;
 import likelion.springsecurityexercise.domain.dto.UserJoinRequest;
+import likelion.springsecurityexercise.domain.dto.UserLoginRequest;
 import likelion.springsecurityexercise.exception.ErrorCode;
 import likelion.springsecurityexercise.exception.HospitalReviewAppException;
 import likelion.springsecurityexercise.service.UserService;
@@ -13,11 +13,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -74,4 +76,25 @@ class UserControllerTest {
                 .andExpect(status().isConflict());
     }
 
+    @Test
+    @DisplayName("로그인 실패 테스트 - userName 없음")
+    @WithMockUser
+    void login_fail() throws Exception {
+        UserLoginRequest loginRequest = UserLoginRequest.builder()
+                .userName("chulmin")
+                .password("123sdf")
+                .build();
+
+        //가정
+        when(userService.login(any(), any())).thenThrow(new HospitalReviewAppException(ErrorCode.USER_NOT_FOUNDED, ""));
+
+        //mock을 통한 테스트
+        mockMvc.perform(post("/api/v1/users/login")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(loginRequest)))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+
+    }
 }
