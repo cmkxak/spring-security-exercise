a291e7960ae660931d1c475029d7051f35dbf31e
cmkxak <gcm466@naver.com>
2022-11-29 15:40:39 +0900

    [add] : login 한 유저에게 JWT를 활용하여 토큰이 발급되도록 구현

--- src/main/java/likelion/springsecurityexercise/exception/ErrorCode.java
+++ src/main/java/likelion/springsecurityexercise/exception/ErrorCode.java
@@ -10,7 +10,7 @@ public enum ErrorCode {
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "user name is duplicatied"),
     INTERNER_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "internal server error"),
     USER_NOT_FOUNDED(HttpStatus.NOT_FOUND, "not found error"),
-    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "bad Request");
+    INVALID_ID_PASSWORD(HttpStatus.BAD_REQUEST, "bad Request");
 
     private HttpStatus httpStatus;
     private String message;
--- src/main/java/likelion/springsecurityexercise/service/UserService.java
+++ src/main/java/likelion/springsecurityexercise/service/UserService.java
@@ -6,15 +6,22 @@ import likelion.springsecurityexercise.domain.dto.UserJoinRequest;
 import likelion.springsecurityexercise.exception.ErrorCode;
 import likelion.springsecurityexercise.exception.HospitalReviewAppException;
 import likelion.springsecurityexercise.repository.UserRepository;
+import likelion.springsecurityexercise.utils.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
+
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder encoder;
+
+    @Value("${jwt.token.secret}")
+    private String secretKey;
+    private long expireTimeMs = 1000 * 60 * 60;
 
     public UserDto join(UserJoinRequest joinRequest) {
         userRepository.findByUserName(joinRequest.getUserName())
@@ -32,14 +39,18 @@ public class UserService {
 
     public String login(String userName, String password) {
         //userName이 존재하는지 여부 확인
-        userRepository.findByUserName(userName).orElseThrow(
+        User user = userRepository.findByUserName(userName).orElseThrow(
                 () -> new HospitalReviewAppException(ErrorCode.USER_NOT_FOUNDED,
                         String.format("%s는 가입한 적이 없습니다.", userName))
         );
-        //password 일치하는지 확인
 
+        //password 일치하는지 확인
+        if(!encoder.matches(password, user.getPassword())){
+            throw new HospitalReviewAppException(ErrorCode.INVALID_ID_PASSWORD,
+                    String.format("userName 또는 password가 잘못 됐습니다."));
+        }
 
         //위의 두 조건들을 모두 통과하면 토큰 발행
-        return "";
+        return JwtTokenUtil.createToken(userName, secretKey, expireTimeMs);
     }
 }
